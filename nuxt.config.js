b1e933cf1249829a5f1d146abd114b0eb3369e32
Lucas Portet <lucasportet@gmail.com>
2020-04-09 19:00:53 +0200

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,33 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Lucas Portet - Full stack developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: `Lucas Portet, developer & maker. I'm building my ideas.`
+      },
+      { name: 'reply-to', content: 'contact@lucasportet.com' },
+      { name: 'owner', content: 'Lucas Portet, contact@lucasportet.com' },
+      { name: 'url', content: 'http://lucasportet.com' },
+      { name: 'identifier-URL', content: 'http://lucasportet.com' },
+      { name: 'author', content: 'Lucas Portet, contact@lucasportet.com' },
+      { name: 'coverage', content: 'Worldwide' },
+      { name: 'distribution', content: 'Global' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=UA-116920394-2'
+      },
+      {
+        src: '/js/ga.js'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
