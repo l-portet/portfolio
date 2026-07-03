ac334e8b507c9b78e681f8e2d8c4ad3b52ad14ae
Lucas Portet <lucasportet@gmail.com>
2026-07-03 09:55:41 +0200

    feat: add minimal portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <>
+      <pre>
+        <code>
+          Hey, I&apos;m{' '}
+          <a
+            href="https://x.com/lukecarry_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-200 hover:text-white hover:underline"
+          >
+            Lucas
+          </a>{' '}
+          and I build viral consumer apps{' '}
+          <span className="animate-caret-blink text-white">█</span>
+        </code>
+      </pre>
+    </>
+  );
+}
